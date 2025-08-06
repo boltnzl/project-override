c79a9e3db64dc8063c2022525dcf36fa7529dba4
boltnzl <159869641+boltnzl@users.noreply.github.com>
2025-08-06 18:16:43 +1200

    Collisions Properly work with bullets

--- bullet.gd
+++ bullet.gd
@@ -1,16 +1,23 @@
 extends Node3D
 
-const SPEED = 200.0
+const SPEED = 100.0
 
 @onready var mesh = $MeshInstance3D
+@onready var particles = $GPUParticles3D
 @onready var ray = $RayCast3D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	var distance = SPEED * delta
+	ray.target_position = Vector3(0, 0, -distance)
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
+	if ray.is_colliding():
+		mesh.visible = false
+		particles.emitting = true
+		await get_tree().create_timer(1.0).timeout
+		queue_free()
+dd
+func _on_timer_timeout() -> void:
+	queue_free()
