29450c9bffb76ee194fae53e08a6bf3b2cc1ee12
boltnzl <159869641+boltnzl@users.noreply.github.com>
2025-08-24 22:35:18 +1200

    stamina fixed

--- bullet.gd
+++ bullet.gd
@@ -7,17 +7,18 @@ const SPEED = 100.0
 @onready var ray = $RayCast3D
 
 func _ready() -> void:
-	pass 
+	# Make sure particles are set to one_shot so they stop automatically
+	particles.one_shot = true
+	particles.emitting = false
 
 func _physics_process(delta: float) -> void:
 	var distance = SPEED * delta
 	ray.target_position = Vector3(0, 0, -distance)
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
+	
 	if ray.is_colliding():
 		mesh.visible = false
 		particles.emitting = true
-		await get_tree().create_timer(1.0).timeout
+		# Wait for full lifetime before freeing
+		await get_tree().create_timer(particles.lifetime).timeout
 		queue_free()
-dd
-func _on_timer_timeout() -> void:
-	queue_free()
--- scripts/Stamina.gd
+++ scripts/Stamina.gd
@@ -1,27 +1,26 @@
 extends ProgressBar
 
-var can_regen = false
-var regen_delay = 0.5
+var regen_delay = 0.5       
 var regen_timer = 0.0
+var min = 20.0             
+var exhausted = false       
 
+var drainrate = 0.5
+var regenerate = 0.4
 
-# Sets max value at the start of game
 func _ready():
-    value = max_value
+	value = max_value
 
-
-# Sets max value at the start of game
 func _process(delta):
-    if Input.is_action_pressed("sprint") and value > 0:
-        value -= 0.5
-        can_regen = false
-        regen_timer = 0.0
-    
-    else:
-        if !can_regen:
-            regen_timer += delta
-            if regen_timer >= regen_delay:
-                can_regen = true
-        
-        if can_regen and value < max_value:
-            value += 0.4
+	if Input.is_action_pressed("sprint") and value > 0 and !exhausted:
+		value -= drainrate
+		regen_timer = 0.0
+		if value <= 0:
+			exhausted = true
+	else:
+		if regen_timer < regen_delay:
+			regen_timer += delta
+		elif value < max_value:
+			value += regenerate 
+			if exhausted and value >= min:
+				exhausted = false
