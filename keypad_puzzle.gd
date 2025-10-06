97e9c31f9a4b66d949cab6d2b5b7e053cb608719
boltnzl <159869641+boltnzl@users.noreply.github.com>
2025-10-07 01:47:11 +1300

    start of keypad puzzle

---
+++ keypad_puzzle.gd
@@ -0,0 +1,97 @@
+extends Control
+
+var typed_text : String
+var passcode : String = "5219"
+var timer = Timer.new()
+@onready var label : RichTextLabel = $VBoxContainer/RichTextLabel
+@onready var square : Sprite2D = $Sprite2D
+
+
+func _ready() -> void:
+	timer.autostart = false
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_Timeout)
+	add_child(timer)
+
+
+func _on_timer_Timeout():
+	square.modulate = Color (1,1,1)
+	typed_text = ""
+	label.text = typed_text
+
+func _on_1_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "1"
+		label.text = typed_text
+
+
+func _on_2_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "2"
+		label.text = typed_text
+
+
+func _on_3_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "3"
+		label.text = typed_text
+
+
+func _on_4_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "4"
+		label.text = typed_text
+
+
+func _on_5_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "5"
+		label.text = typed_text
+
+
+func _on_6_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "6"
+		label.text = typed_text
+
+
+func _on_7_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "7"
+		label.text = typed_text
+
+
+func _on_8_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "8"	
+		label.text = typed_text
+
+
+func _on_9_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "9"
+		label.text = typed_text
+
+
+func _on_0_pressed() -> void:
+	if typed_text.length() < 4:
+		typed_text += "0"
+		label.text = typed_text
+
+
+func _on_clear_pressed() -> void:
+	typed_text = ""
+	label.text = typed_text
+
+
+func _on_enter_pressed() -> void:
+	if typed_text == passcode:
+		square.modulate = Color(0,1,0)
+		typed_text = "Correct"
+		label.text = typed_text
+		timer.start(0.5)
+	else :
+		square.modulate = Color(1,0,0)
+		typed_text = "Incorrect"
+		label.text = typed_text
+		timer.start(0.5)
