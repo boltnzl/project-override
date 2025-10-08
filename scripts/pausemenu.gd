663375a73582e2ca71d071dc861ef579280a205b
boltnzl <159869641+boltnzl@users.noreply.github.com>
2025-10-09 12:03:42 +1300

    pause menu finished

--- scripts/pausemenu.gd
+++ scripts/pausemenu.gd
@@ -1,42 +1,45 @@
 extends Control
 
-
-@onready var resume_button = $Panel/VBoxContainer/Resume
-@onready var settings_button = $Panel/VBoxContainer/Settings
-@onready var main_menu_button = $"Panel/VBoxContainer/Main Menu"
+@onready var health_stamina_ui = $"../Health_Stamina"
+@onready var ammo_ui = $"../AmmoCounter"
 
 
 func _ready():
 	visible = false
-	resume_button.pressed.connect(_on_resume_pressed)
-	settings_button.pressed.connect(_on_settings_pressed)
-	main_menu_button.pressed.connect(_on_main_menu_pressed)
-
-
-func _on_resume_pressed():
-	toggle_pause() 
-
-
-func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://scenes/settings.tscn")
-
-
-func _on_main_menu_pressed():
-	get_tree().paused = false 
-	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
 
 
 func toggle_pause():
 	if visible:
 		get_tree().paused = false
 		visible = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		if health_stamina_ui:
+			health_stamina_ui.visible = true
+		if ammo_ui:
+			ammo_ui.visible = true
 	else:
 		get_tree().paused = true
 		visible = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) 
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		if health_stamina_ui:
+			health_stamina_ui.visible = false
+		if ammo_ui:
+			ammo_ui.visible = false
 
 
 func _input(event):
 	if event.is_action_pressed("escape") and not GameData.puzzle_open:
 		toggle_pause()
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+
+func _on_resume_pressed() -> void:
+	toggle_pause()
